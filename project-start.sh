be67a811a3d1545bcad20224ab49f2ab54c54e1f
rauch <rauch@rising-systems.de>
2022-10-31 16:00:49 +0000

    Update configuration

--- project-start.sh
+++ project-start.sh
@@ -2,11 +2,13 @@
 set -e
 
 function startProject() {
-    docker-compose  -f .devcontainer/docker-compose.yml -f docker-compose.yml up --remove-orphans
+    # docker-compose -f docker-compose.yml -f .devcontainer/docker-compose.yml up --remove-orphans
+    docker-compose -f docker-compose.yml up --remove-orphans
 }
 
 function stopProject() {
-    docker-compose  -f .devcontainer/docker-compose.yml -f docker-compose.yml down
+    # docker-compose -f docker-compose.yml -f .devcontainer/docker-compose.yml down
+    docker-compose -f docker-compose.yml down
 }
 
 startProject
